ebc271ef102f8f70a75533174c3c6e577add78b2
Faisal Nadeem <mfaisalnadeem@hotmail.com>
2021-04-22 23:41:47 +0500

    fixed routing

--- example/src/App.js
+++ example/src/App.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 import AugmentRouter from '@react-augment/react-router'
 
+const Home = () => (<div>Home</div>);
+const About = () => (<div>About</div>);
+
 const App = () => {
-  return <AugmentRouter text="Create React Library Example 😄" />
+  return <AugmentRouter routes={[
+    { path: '/', component: Home },
+    { path: '/about', component: About }
+  ]}/>
 }
 
 export default App
