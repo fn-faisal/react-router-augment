da59c0005ffbd12b427f59fabc67771f6c2dc148
Faisal Nadeem <mfaisalnadeem@hotmail.com>
2021-04-25 04:59:49 +0500

    pre test

--- example-ts/src/components/test.ts
+++
@@ -1,5 +0,0 @@
-function func() {}
-
-Reflect.set(func, 'augId', 'test');
-
-export default func;
--- src/hof/augment-component.hof.tsx
+++ src/hof/augment-component.hof.tsx
@@ -1,10 +1,15 @@
+import React, { ReactNode } from "react";
 import { AugmentComponent } from "../classes/component.class";
 import { AugmentComponentRouteType } from "../types";
 
+type PropsWithChildren<P> = P & { children?: ReactNode };
+
 export function augmentComponent( route: AugmentComponentRouteType ){
   // singleton to register routes.
-  return function({ children }: any){
+  return function<T>(component: React.FC<PropsWithChildren<T>>){
     const augCmp = AugmentComponent.instance();
-    augCmp.addRoute(route, children);
+    // @ts-ignore
+    augCmp.addRoute(route, component.children);
+    return component;
   }
 }
